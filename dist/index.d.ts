1066ada78015e8ac64118f200128a3f6050250a5
Tarun Mamidi <tkmamidi@gmail.com>
2025-04-02 11:51:16 -0500

    Initial commit of TypeScript VRS ID Generator

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * VRS ID Generator
- *
- * A TypeScript package for generating and parsing VRS (Variant Representation Specification)
- * identifiers for genomic variants.
- */
-export { normalizeChromosome, generateVrsId, isValidVrsId, parseVrsId, getChromosomeFromVrsId, getSqlTableForVariant, buildVariantQuery } from './generator';
